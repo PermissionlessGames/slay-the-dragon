be4a6c1db72e899cceffb97cff89c3174cd8802d
Neeraj Kashyap <nkashy1@gmail.com>
2025-02-01 12:39:02 -0800

    Started working on Dragons
    Starting with minting the Dragons.

---
+++ web3/src/Dragons.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { ERC721 } from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import { IERC721 } from "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import { ERC721Enumerable } from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import { IERC721Enumerable } from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import { IERC721Metadata } from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import { IStaker } from "../lib/protocol/web3/contracts/interfaces/IStaker.sol";
+
+/**
+ * @notice Dragons implements the game mechanics for Slay the Dragon!, a permissionless game.
+ * @notice Dragons is an ERC721 contract and NFTs on this contract represent Dragons. The owner of a Dragon
+ * has access to its Hoard.
+ */
+contract Dragons is ERC721Enumerable {
+    error OneDragonAtATime();
+    error InsufficientValueForMint();
+    error InvalidMintTime(uint256 lowerBound);
+
+    uint256 constant SECONDS_PER_DAY = 86400;
+
+    uint256 public CurrentDragon;
+    uint256 public LastDragon;
+    uint256 public LastDragonSlainAt;
+    uint256 public LastDragonMintPrice;
+
+    constructor() ERC721("Dragons", "DRAGON") {}
+
+    function _oneDragonAtATime() internal view {
+        if (CurrentDragon != LastDragon) {
+            revert OneDragonAtATime();
+        }
+    }
+
+    function nextDragonMintPrice(uint256 mintTime) public view returns (uint256 nextDragonID, uint256 mintPrice) {
+        _oneDragonAtATime();
+
+        uint256 auctionStartPrice = 2 * LastDragonMintPrice;
+        if (auctionStartPrice < 20000 ether) {
+            auctionStartPrice = 20000 ether;
+        }
+
+        if (mintTime < LastDragonSlainAt) {
+            revert InvalidMintTime(LastDragonSlainAt);
+        }
+        uint256 daysSinceLastMint = (mintTime - LastDragonSlainAt)/SECONDS_PER_DAY;
+
+        if (daysSinceLastMint * (1000 ether) >= auctionStartPrice - (400 ether)) {
+            mintPrice = 400 ether;
+        } else {
+            mintPrice = auctionStartPrice - 1000 * daysSinceLastMint;
+        }
+
+        nextDragonID = CurrentDragon + 1;
+    }
+
+    function mint() public payable returns (uint256) {
+        // The "nextDragonMintPrice" call also checks that the next dragon can be minted.
+        (uint256 nextDragon, uint256 requiredValue) = nextDragonMintPrice(block.timestamp);
+        if (msg.value < requiredValue) {
+            revert InsufficientValueForMint();
+        }
+
+        CurrentDragon++;
+        LastDragonMintPrice = msg.value;
+
+        _mint(address(this), nextDragon);
+
+        return nextDragon;
+    }
+}
---
+++ web3/test/Dragons.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Dragons} from "../src/Dragons.sol";
+
+contract DragonsTest is Test {
+    Dragons game;
+
+    uint256 minterPrivateKey = 0x1337;
+    address minter = vm.addr(minterPrivateKey);
+
+    function setUp() public {
+        game = new Dragons();
+    }
+
+    function test_deployment() public {
+        assertEq(game.LastDragon(), 0);
+        assertEq(game.CurrentDragon(), 0);
+        assertEq(game.LastDragonSlainAt(), 0);
+        assertEq(game.LastDragonMintPrice(), 0);
+    }
+
+    function test_mint_first_dragon() public {
+        (uint256 nextDragon, uint256 nextDragonMintPrice) = game.nextDragonMintPrice(block.timestamp);
+        assertEq(nextDragonMintPrice, 20000 ether);
+        assertEq(nextDragon, 1);
+
+        vm.deal(minter, nextDragonMintPrice + 100 ether);
+
+        uint256 mintPrice = nextDragonMintPrice + 1 ether;
+
+        uint256 gameBalance0 = address(game).balance;
+        uint256 minterBalance0 = minter.balance;
+        assertEq(game.totalSupply(), 0);
+        assertEq(game.LastDragon(), 0);
+        assertEq(game.CurrentDragon(), 0);
+        assertEq(game.LastDragonSlainAt(), 0);
+        assertEq(game.LastDragonMintPrice(), 0);
+
+        vm.startPrank(minter);
+        uint256 newDragon = game.mint{value: mintPrice}();
+        vm.stopPrank();
+
+        assertEq(address(game).balance, gameBalance0 + mintPrice);
+        assertEq(minter.balance, minterBalance0 - mintPrice);
+        assertEq(game.totalSupply(), 1);
+        assertEq(game.ownerOf(newDragon), address(game));
+        assertEq(game.LastDragon(), 0);
+        assertEq(game.CurrentDragon(), newDragon);
+        assertEq(game.LastDragonSlainAt(), 0);
+        assertEq(game.LastDragonMintPrice(), mintPrice);
+    }
+}
